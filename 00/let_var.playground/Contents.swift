365a1cdf8d6278c9ca734932079e7159f099e074
thee-falcon <omar.makran.10@gmail.com>
2024-01-29 02:38:40 +0100

    declare variables and const variables using var and let

---
+++ 00/let_var.playground/Contents.swift
@@ -0,0 +1,20 @@
+// import: allows you to use classes, functions, and other types defined in the Foundation framework within your Swift code
+// UIKit: is used to include the UIKit framework, which is a fundamental framework for building user interfaces
+import UIKit
+
+// var: to declare a variable, we can change the value of variable(greeting)
+var greeting = "Hello, World!"
+var name = "Omar"
+name = "anouar"
+// let is a constant, you cannot change the value of variable
+let movie = "life is short"
+
+var fullName = "Omar Makran"
+print(fullName)
+fullName = "Makran Omar"
+print("fullName =>", fullName)
+let nickName = "the falcon"
+let userName = "omakran"
+print("nickName =>", nickName)
+print("userName =>", userName)
+print("the best movie =>", movie)
